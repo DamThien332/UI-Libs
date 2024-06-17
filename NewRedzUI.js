69437f1a005233b49e8069c9e0ba68de44593630
LitealDamThien <luizfelipegamermcpe388@gmail.com>
2024-06-17 13:55:04 -0300

    Update NewRedzUI.js

--- NewRedzUI.js
+++ NewRedzUI.js
@@ -36,16 +36,16 @@ local redzlib = {
       ["Color Dark Text"] = Color3.fromRGB(190, 190, 190)
     },
     DamThien = {
-      ["Color Hub 1"] = ColorSequence.new({
-        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)),
+       ["Color Hub 1"] = ColorSequence.new({
+        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(25, 25, 25)),
         ColorSequenceKeypoint.new(0.50, Color3.fromRGB(32.5, 32.5, 32.5)),
         ColorSequenceKeypoint.new(1.00, Color3.fromRGB(25, 25, 25))
       }),
-      ["Color Hub 2"] = Color3.fromRGB(45, 45, 45),
-      ["Color Stroke"] = Color3.fromRGB(45, 45, 45),
-      ["Color Theme"] = Color3.fromRGB(0, 0, 0),
-      ["Color Text"] = Color3.fromRGB(245, 245, 245),
-      ["Color Dark Text"] = Color3.fromRGB(190, 190, 190)
+      ["Color Hub 2"] = Color3.fromRGB(30, 30, 30),
+      ["Color Stroke"] = Color3.fromRGB(40, 40, 40),
+      ["Color Theme"] = Color3.fromRGB(0, 255, 255),
+      ["Color Text"] = Color3.fromRGB(243, 243, 243),
+      ["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
     },
     Purple = {
       ["Color Hub 1"] = ColorSequence.new({
